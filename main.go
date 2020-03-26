84abc3f83e785213e85c1074ca8976ac53c07c18
Mitesh Shah <miteshninja@gmail.com>
2020-03-26 11:27:03 +0530

    feature: basic structure for api

--- main.go
+++ main.go
@@ -1,7 +1,109 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// SupertokensCore main
+type SupertokensCore struct {
+	backends         *[]backendConfig
+	handshakeInfo    *HandshakeInfo
+	deviceDriverInfo *DeviceDriverInfo
+}
+
+// DeviceDriverInfo info about device and driver
+type DeviceDriverInfo struct {
+	frontendSDKList *[]frontendSDK
+	driver          *driver
+}
+
+type driver struct {
+	name    string
+	version string
+}
+type frontendSDK struct {
+	name    string
+	version string
+}
+
+type backendConfig struct {
+	hostname string
+	port     int
+}
+
+// Querier allows transparently calling multiple backends
+type Querier struct {
+}
+
+// HandshakeInfo singleton
+type HandshakeInfo struct {
+	jwtSigningPublicKey            string
+	cookieDomain                   string
+	cookieSecure                   bool
+	accessTokenPath                string
+	refreshTokenPath               string
+	enableAntiCsrf                 bool
+	accessTokenBlacklistingEnabled bool
+	jwtSigningPublicKeyExpiryTime  int
+}
 
 func main() {
-	fmt.Println("vim-go")
+	stCore := &SupertokensCore{}
+	hello()
+	stCore.backends = &[]backendConfig{
+		backendConfig{
+			"localhost", 3567,
+		},
+		backendConfig{
+			"localhost", 3568,
+		},
+	}
+	stCore.deviceDriverInfo = &DeviceDriverInfo{
+		frontendSDKList: &[]frontendSDK{
+			frontendSDK{
+				"vuejs", "1.1",
+			},
+			frontendSDK{
+				"react", "1.0",
+			},
+		},
+		driver: &driver{
+			"supertokens-go", "0.0",
+		},
+	}
+	stCore.init()
+}
+
+func hello() *http.Response {
+	resp, err := http.Get("http://localhost:3567/hello")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return resp
+}
+
+func (st *SupertokensCore) init() {
+	for _, backend := range *st.backends {
+		log.Println(backend)
+	}
+	// Perform Handshake
+	if st.handshakeInfo == nil {
+		resp, err := http.Get("http://localhost:3567/handshake")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Println(string(body))
+	}
+}
+
+func handshake() {
 }
