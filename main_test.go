71a2866833ee5db247d976fac3b814aac72bdfc8
Mitesh Shah <miteshninja@gmail.com>
2020-03-26 17:49:01 +0530

    refactor: session test refactored out

--- main_test.go
+++ main_test.go
@@ -99,38 +99,3 @@ func TestInit(t *testing.T) {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
-
-func TestGetSession(t *testing.T) {
-	stCore := &SupertokensCore{}
-	backends := []BackendConfig{
-		BackendConfig{
-			"localhost", 3567,
-		},
-		BackendConfig{
-			"localhost", 3568,
-		},
-	}
-	deviceDriverInfo := &DeviceDriverInfo{
-		frontendSDKList: []FrontendSDK{
-			FrontendSDK{
-				"vuejs", "1.1",
-			},
-		},
-	}
-	stCore.init(backends, deviceDriverInfo)
-
-	jwtPayload := &map[string]interface{}{
-		"userId": "User1",
-		"name":   "spooky action at a distance",
-	}
-
-	sessionData := &map[string]interface{}{
-		"awesomeThings": []string{
-			"ox", "oxalorg", "programming", "supertokens",
-		},
-	}
-	_, err := stCore.createSession("User1", jwtPayload, sessionData)
-	if err != nil {
-		t.Errorf("createSession() failed with errors %v", err)
-	}
-}
