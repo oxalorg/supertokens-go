631eee80eb5c9a4f2cd3e8dbd2b7ba797d38312c
Mitesh Shah <miteshninja@gmail.com>
2020-03-26 15:23:36 +0530

    test: added tests for handshake api

--- main_test.go
+++ main_test.go
@@ -4,7 +4,55 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	stCore := &SupertokensCore{}
+	backends := []BackendConfig{
+		BackendConfig{
+			"localhost", 3568,
+		},
+		BackendConfig{
+			"localhost", 3567,
+		},
+	}
+	deviceDriverInfo := &DeviceDriverInfo{
+		frontendSDKList: []FrontendSDK{
+			FrontendSDK{
+				"vuejs", "1.1",
+			},
+			FrontendSDK{
+				"react", "1.0",
+			},
+		},
+	}
+	stCore.init(backends, deviceDriverInfo)
 	resp := stCore.hello()
+	got := resp.StatusCode
+	want := 200
+	if got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
+
+func TestHandshake(t *testing.T) {
+	stCore := &SupertokensCore{}
+	backends := []BackendConfig{
+		BackendConfig{
+			"localhost", 3568,
+		},
+		BackendConfig{
+			"localhost", 3567,
+		},
+	}
+	deviceDriverInfo := &DeviceDriverInfo{
+		frontendSDKList: []FrontendSDK{
+			FrontendSDK{
+				"vuejs", "1.1",
+			},
+			FrontendSDK{
+				"react", "1.0",
+			},
+		},
+	}
+	stCore.init(backends, deviceDriverInfo)
+	resp := stCore.handshake()
 	got := resp.StatusCode
 	want := 200
 	if got != want {
